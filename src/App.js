581e77aa1608f1f3c6cfc828a2f0feabc06492d3
Rafael Valentim <rmoraesvalentim@gmail.com>
2021-01-18 17:14:02 -0300

    Teste 2

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   const handleInputChange = (event, newInputValue) => {
 
     setInputValue(newInputValue);    
-
+    console.log('2');
     
     if( newInputValue.length >= 4 ) {
 
